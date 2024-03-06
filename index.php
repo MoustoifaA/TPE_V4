2919a5f3c1b05b912dddf3e7a4a80d8b0038cdfe
Moustoifaa <moustoifa.amadi@gmail.com>
2024-03-06 18:05:27 +0100

    index.php ets style.css

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceuil</title>
+
+    <link rel="stylesheet" href="/css/reset.css">
+    <link rel="stylesheet" href="/css/style.css">
+
+</head>
+
+<body>
+
+    <main>
+
+        <head>
+
+        </head>
+
+        <section>
+
+        </section>
+        <footer>
+
+        </footer>
+
+    </main>
+
+
+</body>
+
+</html>
