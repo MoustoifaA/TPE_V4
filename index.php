3d2841c20e3d6c8fdc0e81ffa3f730d649e267e0
Moustoifaa <moustoifa.amadi@gmail.com>
2024-03-06 20:39:09 +0100

    texte 2 style article et general

--- index.php
+++ index.php
@@ -19,14 +19,13 @@
   
 </head>
 
-<body class="container">
+<body class="w-75 position-absolute top-0 start-50 translate-middle-x">
 
-<div class="w-75 position-absolute top-0 start-50 translate-middle-x"> 
-    <header >
-        <h1>
+    <header style="background-color: #75CAFA;">
+        <h1 class="text-light pt-0 ps-2 mt-0">
             Aviation
         </h1>
-        <nav class="navbar p-2 mb-2" style="background-color: #C9EBFD;">
+        <nav class="navbar p-2 mb-2" >
             
             <a style="--bs-link-hover-color-rgb: 255, 255, 255;" href="#">Accueil</a>
             <a style="--bs-link-hover-color-rgb: 255, 255, 255;" href="#">Histoire</a>
@@ -39,7 +38,7 @@
 
     <main>
         <!-- Main content goes here -->
-        <article>
+        <article  class="p-3 border bg-light card">
         <p>
     Le mot Aviation ( du mot latin avis &laquo;&nbsp;oiseau&nbsp;&raquo; et actio &laquo;&nbsp;action&nbsp;&raquo; )  a &eacute;t&eacute; employer pour la premi&egrave;re fois par <a href="https://fr.wikipedia.org/wiki/Guillaume_Joseph_Gabriel_de_La_Landelle" >Gabriel De La Laudelle</a> en 1863  dans le livre Aviation au navigation a&eacute;rienne sans ballon racontant les  tentatives des vols de Jean-Marie de Bris dans appareil plus lourd que  l&rsquo;aire.</p> 
     
@@ -55,7 +54,7 @@
     <p>N&eacute;anmoins, les proc&eacute;d&eacute;s permettant de faire voler un avion ne sont  pas aussi &eacute;vidents. Ainsi notre TPE tournera autour de la probl&eacute;matique  suivante&nbsp;: 
     </p>
 
-    <p><u>Quels sont les ph&eacute;nom&egrave;nes  et m&eacute;canismes permettant &agrave; un avion de d&eacute;coller  et de le maintenir en l&rsquo;air ?</u> 
+    <p> <u>Quels sont les ph&eacute;nom&egrave;nes  et m&eacute;canismes permettant &agrave; un avion de d&eacute;coller  et de le maintenir en l&rsquo;air ? </u>
     </p>
 
     <p>Pour  r&eacute;pondre &agrave; cette  probl&eacute;matique le plus simplement possible on effectuera des  exp&eacute;riences.
@@ -65,13 +64,111 @@
     </p>
 
     </article>
+
+    <ariticle class="p-3 border bg-light card">
+    <h3>Engin plus dense que l&rsquo;air</h3>
+    <p>&nbsp;&nbsp; Pendant des si&egrave;cles le plus grand r&ecirc;ve de l&rsquo;homme a
+      &eacute;t&eacute; de voler, et ce fut leurs but communs mais seulement quelques-uns se sont immortalis&eacute;s
+      gr&acirc;ce &agrave; leurs brillantes innovations. En effet, Le premier homme, que l&rsquo;on peut
+      consid&eacute;rer comme le p&egrave;re de la navigation a&eacute;rienne fut le <em><a
+          href="Infos_Perso/Sir George Cayley.html">Baronet Georges Cayley</a></em> (1773-1857). Il construisit en 1807 ce que l&rsquo;on
+      consid&egrave;re comme le premier mod&egrave;le r&eacute;duit de planeur. Il s&rsquo;agissait en fait d&rsquo;un
+      manche &agrave; balai sur lequel &eacute;tait mont&eacute;e une aile en forme de cerf-volant. C&rsquo;&eacute;tait
+      suffisant pour faire admettre le principe que l'engin ï¿½tait plus lourd que l&rsquo;air.<br />
+      A partir de ce mod&egrave;le, il con&ccedil;ut en 1849 un autre planeur capable de transporter un jeune
+      gar&ccedil;onnet puis son cochet en 1853. <em>Cayley</em> continua ses recherches jusqu&rsquo;&agrave; sa mort et
+      sugg&eacute;ra plusieurs id&eacute;es :</p>
+    <p> - utiliser un moteur &agrave; combustion, prouvant qu&rsquo;une surface courbe poss&eacute;dait une
+      surface ascensionnelle propre<br />
+      - des ailes biplans ou triplans offraient une force ascensionnelle propre optimum pour une structure
+      l&eacute;g&egrave;re et robuste.</p>
+    <p>Bien qu&rsquo;&agrave; cette &eacute;poque, on savait &agrave; peu pr&egrave;s construire un engin
+      capable de voler, le probl&egrave;me du pilotage demeurait entier : le moteur appropri&eacute; restait &agrave;
+      inventer. Il faut enfin exp&eacute;rimenter en 1876 par l&rsquo;ing&eacute;nieur allemand <em><a
+          href="Infos_Perso/Nicholas Otto.html">Nicholas Otto</a></em>.</p>
+    <p> Mais l&rsquo;invention d&rsquo;<em>Otto</em> appartenait au futur. Les contraintes de
+      l&rsquo;&eacute;poque permirent de d&eacute;velopper l&rsquo;a&eacute;rodynamisme de la carcasse des appareils
+      pour essayer de d&eacute;couvrir le moyen de contr&ocirc;ler leurs engins dans les airs.</p>
+    <p> ( Vid&eacute;o, Image, et Photo ou croquis&hellip;)</p>
+
+    <h3> Les D&eacute;buts du Vol </h3>
+    <p>&nbsp;&nbsp;Le vol libre a &eacute;t&eacute; initi&eacute; par <a href="Infos_Perso/Otto Lilienthal.html">Otto Lilienthal</a> (1848-1896). Ce constructeur de planeurs r&eacute;alisa de nombreux vols sur
+      des engins particuli&egrave;rement bien con&ccedil;us. Scientifique, il construisait, modifiait et notait le fruit
+      de ses recherches. Il construisait des planeurs &agrave; ailes de type delta afin d&rsquo;assurer une bonne
+      r&eacute;partition autour du centre de gravit&eacute;. Il assurait le contr&ocirc;le de l&rsquo;appareil par le
+      mouvement du corps.</p>
+    <p>Lilienthal fut un pr&eacute;curseur et influen&ccedil;a d&rsquo;autres chercheurs tels que Percy
+      Pilcher (1866-1899) ou Octave Chanute (1832-1910). Ce dernier, un constructeur am&eacute;ricain, publia un recueil
+      intitul&eacute; &laquo; Progr&egrave;s &raquo; dans les machines volantes. Son livre, ses conseils et son
+      amiti&eacute; inspira les fr&egrave;res Orville et Wilbur Wright.</p>
+    <p>Beaucoup d&rsquo;autres personnes ont permis de faire &eacute;voluer les technologies de vol.
+      Parmi ceux-ci, on peut citer l&rsquo;Allemand Gottlieb Daimler qui r&eacute;alisa le premier moteur d&rsquo;avion
+      en 1885. Il s&rsquo;agissait d&rsquo;un monocylindre &agrave; combustion interne. Il s&rsquo;inspirait des moteurs
+      &agrave; quatre temps invent&eacute;s par Nicholas Otto et utilisait l&rsquo;essence comme carburant.</p>
+    <p>Le premier vol motoris&eacute;, contrairement &agrave; ce qu&rsquo;on pense, ne fut pas
+      r&eacute;alis&eacute; par Cl&eacute;ment Ader en 1890. L&rsquo;Eole (nom de l'avion) ne fit que se soulever mais
+      ne vola pas. Cette nuance n&rsquo;enl&egrave;ve rien &agrave; l&rsquo;exploit qu&rsquo;a r&eacute;alis&eacute; le
+      pionnier fran&ccedil;ais.</p>
+    <p>L&rsquo;Am&eacute;ricain Samuel Pierpont Langley fut celui qui faillit devancer les fr&egrave;res
+      Wright. Il collabora avec Charles Manley et Stephen Balzer pour faire voler un avion baptis&eacute;
+      l&rsquo;a&eacute;rodrome, &eacute;quip&eacute; d&rsquo;un remarquable moteur rotatif en &eacute;toile &agrave;
+      cinq cylindres. En d&eacute;pit de cette technologie avanc&eacute;e, l&rsquo;avion de Langley ne vola jamais. Deux
+      essais, le 7 octobre et le 8 d&eacute;cembre se termin&egrave;rent dans le Potomac. Les observateurs
+      rapport&egrave;rent que l&rsquo;avion se lib&eacute;ra mal de sa rampe de lancement. Certains ont avanc&eacute;
+      que la structure de l&rsquo;appareil se d&eacute;sint&eacute;gra au d&eacute;collage.</p>
+    <p>L&rsquo;exploit tant attendu eut lieu le jeudi 17 d&eacute;cembre 1903. Les fr&egrave;res Wright
+      ont m&eacute;rit&eacute; leurs succ&egrave;s, gr&acirc;ce &agrave; leur d&eacute;termination &agrave; surmonter
+      toutes les difficult&eacute;s qu&rsquo;ils rencontraient. Lorsque le &laquo; Flyer &raquo; fut
+      d&eacute;mont&eacute; apr&egrave;s ce jour historique, c&rsquo;&eacute;tait la fin de la premi&egrave;re phase de
+      vol motoris&eacute;. Trois autres vols survirent, le dernier fut le plus performant avec une distance couverte de
+      260 m&egrave;tres, le gouvernail de profondeur fut alors endommag&eacute; &agrave; l&rsquo;atterrissage. Ces voles
+      eurent lieu sans t&eacute;moin et le monde ne fut pas inform&eacute; qu&rsquo;un homme avait vol&eacute; dans une
+      machine plus lourde que l&rsquo;air.</p>
+    <p>Ce n&rsquo;est pas avant novembre 1906, trois ann&eacute;es plus tard, que le Br&eacute;silien
+      Albertos Santos-Dumont r&eacute;volutionna l&rsquo;Europe en faisant voler son &laquo; 14+ bis &raquo; sur 61
+      m&egrave;tres.</p>
+    <p>18 mois apr&egrave;s le premier vol des fr&egrave;res Wright, une am&eacute;lioration du Flyer
+      boucle 40 km. Dor&eacute;navant leur machine ob&eacute;it au doigt et &agrave; l'oeil. De peur de la concurrence
+      ils taisent leur performances mais lorsqu'ils d&eacute;cident de faire un vol en public les Fran&ccedil;ais sont
+      entr&eacute;s en lice.</p>
+    <p>Peu &agrave; peu on oublie les biplans par manque de vitesse et certains hommes comme
+      Bl&eacute;riot Louis ou Roland Garros utilisent des monoplans pour r&eacute;aliser de v&eacute;ritables exploits;
+      la travers&eacute;e de la mer M&eacute;diterran&eacute;e par Garros ! D'autres exploits suivent et l'aviation
+      commence &agrave; devenir utile et civile avec les premiers avions de ligne dans les ann&eacute;es 1930. C'est
+      &agrave; cette &eacute;poque qu'arrivent les DC-3 ainsi que la cr&eacute;ation d'une firme connue de tous: Air
+      France.</p>
+    <p>Les avions sont utilis&eacute;s pour des fins militaires durant les guerres. Dans le m&ecirc;me
+      moment on voit apparaitre les r&eacute;acteurs dans les ann&eacute;es 40 et le mur du son est franchi par Chuck
+      Yeager le 14 Octobre 1947.</p>
+
+    <h3> Les Avions de nos Jours</h3>
+    <p>&nbsp;&nbsp;De nos jours, les avions sont tr&egrave;s performants et capables de traverser
+      l'oc&eacute;an Atlantique en 3H30 avec le Concorde ou encore de r&eacute;aliser des d&eacute;collages et
+      atterrissage verticaux avec le XFY-1 ! La p&eacute;riode 1958-1970 est marqu&eacute;e par l'exploration du domaine
+      supersonique (dont la vitesse est sup&eacute;rieure &agrave; la vitesse du son) et hypersonique (la vitesse de
+      Mach 6 est atteinte par Wright en 1961), et par le vol des premiers avions civiles supersoniques (Tupolev 144 en
+      1968 et Concorde en 1969). Apr&egrave;s le premier choc p&eacute;trolier (1973), le co&ucirc;t extr&ecirc;me de
+      vol tels que ceux du Concorde a mis fin &agrave; de telles r&eacute;alisations. Ainsi, le record de vitesse sur un
+      parcours commercial &eacute;tabli en 1980 par le Concorde (New York - Londres en 2h59, soit une moyenne de 1886
+      km/h.) n'a pas &eacute;t&eacute; battu. L'&eacute;volution se caract&eacute;rise par l'affinement de l'assistance
+      informatique qui permet de r&eacute;duire les &eacute;quipes de pilotages. De plus, ces m&ecirc;mes
+      syst&egrave;mes informatiques permettent d'assurer un meilleur contr&ocirc;le de l'avion (atterrissage automatique
+      en temps de pluie, nuit ou brouillard par exemple; direction automatique; etc.).</p>
+    <p>A titre d'information, les deux principaux constructeurs d'avions civils dans le monde sont
+      Airbus (Toulouse, Europe) et Boeing (Seattle, USA). Ces deux grandes compagnies se partagent le march&eacute;
+      mondial de l'aviation civile. </p>
+    <p>&nbsp;</p>
+    </ariticle>
+    <ariticle class="p-3 border bg-light card"></ariticle>
+    <ariticle class="p-3 border bg-light card"></ariticle>
+    <ariticle class="p-3 border bg-light card"></ariticle>
+
     </main>
 
-    <footer >
-        <p>Copyright NoobLife 2024, tous droits réservés.</p>
+    <footer class="p-3 mt-3 border bg-light card " >
+        <p class="position-absolute top-50 start-50 translate-middle">Copyright NoobLife 2024, tous droits réservés.</p>
     </footer>
 
-</div>
 
     <!-- bootstrap js -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
