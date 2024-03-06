a8a7b2bd9ef5d0999e6b916ccc701b9eca815d8e
Moustoifaa <moustoifa.amadi@gmail.com>
2024-03-06 18:30:54 +0100

    structure code html sur base V3 nav

--- index.php
+++ index.php
@@ -3,29 +3,36 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Acceuil</title>
+    <title>TPE - Accueil</title>
+    <meta name="generator" content="TPE Aviation">
+    <meta name="author" content="Moustoifa">
 
+    <meta name="keywords" content="avion, aviation, portance, tourbillon, aile, ailles, TPE, tpe, moteur, lycée, equation de portance, turbine, bernouillie">
+    <meta name="description" content="TPE d'une premiere s sur l'aviation, Quels sont les phénomènes et mécanismes permettant à un avion de décoller et de le maintenir en l'air?">
+    <meta name="robots" content="index">
+  
 </head>
 
 <body>
+    <header>
+        <nav >
+            <ul>
+                <li><a href="Home.html">Accueil</a></li>
+                <li><a href="Histoire.html">Histoire</a></li>
+                <li><a href="Physique-de-lavion.html">Physique de l'avion</a></li>
+                <li><a href="Conclusion.html">Conclusion</a></li>
+                <li><a href="Nous.html">Nous</a></li>
+            </ul>
+        </nav>
+    </header>
 
-    <main>
-
-        <head>
-
-        </head>
-
-        <section>
-
-        </section>
-        <footer>
-
-        </footer>
-
+    <main >
+        <!-- Main content goes here -->
     </main>
 
-
+    <footer >
+        <p>Copyright MX Corporation 2008, tous droits réservés</p>
+    </footer>
 </body>
 
 </html>
