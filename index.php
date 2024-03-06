38b34c957edf73b79bdeaaf40b4c18b5ff3dfe6f
Moustoifaa <moustoifa.amadi@gmail.com>
2024-03-06 18:44:10 +0100

    structure avec body centré

--- index.php
+++ index.php
@@ -10,11 +10,17 @@
     <meta name="keywords" content="avion, aviation, portance, tourbillon, aile, ailles, TPE, tpe, moteur, lycée, equation de portance, turbine, bernouillie">
     <meta name="description" content="TPE d'une premiere s sur l'aviation, Quels sont les phénomènes et mécanismes permettant à un avion de décoller et de le maintenir en l'air?">
     <meta name="robots" content="index">
+
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   
 </head>
 
-<body>
+<body class="w-75 position-absolute top-0 start-50 translate-middle-x">
     <header>
+        <h1>
+            Aviation
+        </h1>
         <nav >
             <ul>
                 <li><a href="Home.html">Accueil</a></li>
@@ -28,11 +34,17 @@
 
     <main >
         <!-- Main content goes here -->
+        <article>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse laudantium perferendis iusto quasi, amet dolorem nemo fugit ea quaerat, consequuntur numquam eaque provident aut, quod commodi laborum voluptatem quis laboriosam recusandae! Consequatur dolore voluptates eum quam quae at, cumque sequi nostrum quasi, dolorem cum repellat cupiditate praesentium natus ab quod nobis debitis! Dolorum perferendis eius illum? Totam non harum aspernatur, provident modi commodi iure hic perspiciatis in ea? Voluptates deserunt nostrum nesciunt ducimus excepturi error unde, repellat totam cupiditate rem quos laudantium dolorem aliquid recusandae dolores reiciendis ut natus qui commodi! Eveniet natus quae quod velit eligendi eos facilis, qui ipsa non quibusdam, a dolores. Laborum atque harum ab reiciendis. Eligendi earum quasi nostrum libero eaque modi autem quisquam quod alias aspernatur officia nihil porro quo sapiente, in praesentium amet natus iste omnis? Officia quisquam aliquid sapiente, reiciendis sint quod dolorem consectetur voluptate, ipsam repellendus eos ullam explicabo odio sed fugit ratione nam aliquam quae. Officia totam accusantium eius dolorem quo distinctio repudiandae praesentium impedit, sunt dolores illo ab vel quos perspiciatis fugit aperiam asperiores debitis itaque placeat porro corporis error minima explicabo. Tenetur eum laudantium sapiente rem adipisci fuga perspiciatis nihil aliquam aut enim molestiae, temporibus earum rerum eius quis numquam quibusdam. Commodi, magni veniam. Iusto perferendis quibusdam nesciunt cumque, quos necessitatibus esse fugiat magni voluptatibus voluptates voluptas nostrum libero iste rerum dolore voluptate laborum dolor asperiores quam error, eveniet veritatis suscipit praesentium molestiae! Repellat amet necessitatibus beatae maiores consectetur modi quas soluta commodi illum, repellendus similique. Officia accusantium quos ducimus ab vitae, modi eum laboriosam tempore in, illo labore obcaecati? Nemo voluptatibus accusamus fugit dicta deleniti, numquam sint molestiae! Iusto, qui beatae. Praesentium consequuntur fuga saepe fugit quisquam, adipisci quam reiciendis. Dolorum, repellat reprehenderit est maxime accusamus dolor quibusdam quos illum adipisci ea quia at, inventore aliquid laboriosam!</p>
+        </article>
     </main>
 
     <footer >
         <p>Copyright MX Corporation 2008, tous droits réservés</p>
     </footer>
+
+    <!-- bootstrap js -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
